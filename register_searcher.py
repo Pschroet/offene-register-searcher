b46e2cdcbb6db3fab911ec4139162a604b9e63e3
Pschroet <Pschroet@users.noreply.github.com>
2023-10-05 01:38:24 +0200

    improved code and optimized runtime (redundant unnecessary checks)

--- register_searcher.py
+++ register_searcher.py
@@ -12,36 +12,31 @@ class Register_Searcher(object):
     def search(self, terms=[], allterms=False):
         for entry in self.jsonl_db:
             try:
-                allterms_found = []
+                allterms_found = True
                 entry_data = json.loads(entry)
                 for t in terms:
-                    #defined this way, because of argument allterms
-                    #see condition for print after the loop
-                    term_found = []
+                    term_found = False
                     if "name" in entry_data and t in entry_data["name"]:
-                        term_found.append(True)
-                    else:
-                        term_found.append(False)
-                    if "officers" in entry_data:
+                        term_found = True
+                    elif "officers" in entry_data:
                         for o in entry_data["officers"]:
                             if "name" in o and t in o["name"]:
-                                term_found.append(True)
-                            else:
-                                term_found.append(False)
+                                term_found = True
+                                break
                             if "other_attributes" in o:
                                 for officer_attribute in o["other_attributes"]:
                                     if isinstance(o["other_attributes"][officer_attribute], str) and t in o["other_attributes"][officer_attribute]:
-                                        term_found.append(True)
-                                    else:
-                                        term_found.append(False)
-                    if True in term_found:
-                        if allterms:
-                            allterms_found.append(True)
-                        else:
-                            print("Found term '" + str(t) + "' in line " + str(self.read_line) + ": " + str(entry_data))
-                    elif allterms:
-                        allterms_found.append(False)
-                if allterms and False not in allterms_found:
+                                        term_found = True
+                                        break
+                    if not allterms and term_found:
+                        print("Found term '" + str(t) + "' in line " + str(self.read_line) + ": " + str(entry_data))
+                        #exit loop, because one term has been found, which is enough
+                        break
+                    elif not term_found:
+                        allterms_found = False
+                        #all terms required, it's enough that one hasn't been found
+                        break
+                if allterms and allterms_found:
                     print("Found all terms " + str(terms) + " in line " + str(self.read_line) + ": " + str(entry_data))
                 self.read_line += 1
             except Exception as e:
