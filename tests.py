c05029fe7be4d2354eb241a4b734f4e60ab2043d
Pschroet <Pschroet@users.noreply.github.com>
2023-10-08 23:41:16 +0200

    moved test data out of test class

--- tests.py
+++ tests.py
@@ -4,49 +4,50 @@ import sys
 import unittest
 from io import StringIO
 
+test_data_struct = {
+                    "all_attributes": {
+                        "_registerArt": "MUS",
+                        "_registerNummer": "999999",
+                        "additional_data": {
+                            "AD": True,
+                            "CD": True,
+                            "DK": True,
+                            "HD": False,
+                            "SI": True,
+                            "UT": True,
+                            "VÖ": False
+                        },
+                        "federal_state": "Musterstadt",
+                        "native_company_number": "Musterstadt MUS 999999",
+                        "registered_office": "Musterstadt",
+                        "registrar": "Musterstadt"
+                    },
+                    "company_number": "M1337M_MUS999999",
+                    "current_status": "currently registered",
+                    "jurisdiction_code": "de",
+                    "name": "Muster & Co KG",
+                    "officers": [
+                        {
+                            "name": "Max Mustermann",
+                            "other_attributes": {
+                                "city": "Musterstadt",
+                                "firstname": "Max",
+                                "flag": "vertretungsberechtigt gemäß allgemeiner Vertretungsregelung",
+                                "lastname": "Mustermann"
+                            },
+                            "position": "Geschäftsführer",
+                            "start_date": "2018-12-22",
+                            "type": "person"
+                        }
+                    ],
+                    "registered_address": "Musterstraße 1, 34346 Musterstadt.",
+                    "retrieved_at": "2018-12-23T09:05:00Z"
+                   }
+
 class Test(unittest.TestCase):
 
     def setUp(self):
-        self.test_data_struct = {
-                                    "all_attributes": {
-                                        "_registerArt": "MUS",
-                                        "_registerNummer": "999999",
-                                        "additional_data": {
-                                            "AD": True,
-                                            "CD": True,
-                                            "DK": True,
-                                            "HD": False,
-                                            "SI": True,
-                                            "UT": True,
-                                            "VÖ": False
-                                        },
-                                        "federal_state": "Musterstadt",
-                                        "native_company_number": "Musterstadt MUS 999999",
-                                        "registered_office": "Musterstadt",
-                                        "registrar": "Musterstadt"
-                                    },
-                                    "company_number": "M1337M_MUS999999",
-                                    "current_status": "currently registered",
-                                    "jurisdiction_code": "de",
-                                    "name": "Muster & Co KG",
-                                    "officers": [
-                                        {
-                                            "name": "Max Mustermann",
-                                            "other_attributes": {
-                                                "city": "Musterstadt",
-                                                "firstname": "Max",
-                                                "flag": "vertretungsberechtigt gemäß allgemeiner Vertretungsregelung",
-                                                "lastname": "Mustermann"
-                                            },
-                                            "position": "Geschäftsführer",
-                                            "start_date": "2018-12-22",
-                                            "type": "person"
-                                        }
-                                    ],
-                                    "registered_address": "Musterstraße 1, 34346 Musterstadt.",
-                                    "retrieved_at": "2018-12-23T09:05:00Z"
-                                }
-        self.in_data = StringIO(json.dumps(self.test_data_struct))
+        self.in_data = StringIO(dumps(test_data_struct))
         #redirect the output to be able to check it, to avoid other workarounds or changing the code
         self.out = StringIO()
         sys.stdout = self.out
