5bfc8ed60ebe735ccbe061cc08ff87488741d160
Pschroet <Pschroet@users.noreply.github.com>
2023-10-14 16:14:02 +0200

    added argument to use string search and descriptions to all parameters

--- query_offeneregister.py
+++ query_offeneregister.py
@@ -4,9 +4,10 @@ import register_searcher
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("terms", default=[], nargs="+")
-    argparser.add_argument("-j", "--jsonl", required=True, action="store", default=None)
-    argparser.add_argument("-a", "--allterms", action="store_true", default=False, required=False)
+    argparser.add_argument("terms", default=[], nargs="+", description="terms to search for in the data")
+    argparser.add_argument("-j", "--jsonl", required=True, action="store", default=None, description="file to serve as database")
+    argparser.add_argument("-a", "--allterms", action="store_true", default=False, required=False, description="line must contain all search terms")
+    argparser.add_argument("-s", "--stringsearch", action="store_true", default=False, required=False, description="search lines with string comparison")
     args = argparser.parse_args()
     if not hasattr(args, "jsonl") or args.jsonl is None:
         print("argument jsonl file is missing, usage")
@@ -17,4 +18,5 @@ if __name__ == "__main__":
         else:
             print("Searching for terms " + str(args.terms) + " in database " + str(args.jsonl))
             reg_par = register_searcher.Register_Searcher(jsonl=args.jsonl)
-            reg_par.search_json(terms=args.terms, allterms=args.allterms)
+            if args.stringsearch: reg_par.search_string(terms=args.terms, allterms=args.allterms)
+            else: reg_par.search_json(terms=args.terms, allterms=args.allterms)
